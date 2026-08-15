91c9eef0496f96672ffd42b83dab798c201d737a
manoj <manojmandy35@gmail.com>
2026-08-15 13:46:36 +0530

    mongodb.sh

--- Catalogue.sh
+++ Catalogue.sh
@@ -67,7 +67,7 @@ VALIDATE $? "unziping catalogue"
 npm install &>>$LOG_FILE
 VALIDATE $? "Installing Dependencies"
 
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/Catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "coping catatlogue service"
 
 systemctl daemon-reload &>>$LOG_FILE
