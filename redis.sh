20ceffbe8caecc320567a67bda3a0424255f9307
manoj <manojmandy35@gmail.com>
2026-08-15 15:59:43 +0530

    mongodb.sh

--- redis.sh
+++ redis.sh
@@ -51,7 +51,7 @@ systemctl start redis  &>>$LOG_FILE
 VALIDATE $? "START REDIS"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(($END_TIME - $START_TIME))
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
 
